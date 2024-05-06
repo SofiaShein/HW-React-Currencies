d404481130cc57ef44e5d1d92200fb4e4fe12281
SofiaShein <sheinkinasofiya@gmail.com>
2024-05-06 18:03:00 +0300

    first commit

---
+++ src/components/Currencies.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import Currency from "./Currency";
+import Table from 'react-bootstrap/Table';
+
+function Currencies() {
+  const [currencies, setCurrencies] = useState([]);
+
+  useEffect(() => {
+    fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json').then(res => res.json())
+      .then(data => {
+        setCurrencies(data);
+      });
+  }, []);
+
+  return <>
+    <h1>Currencies</h1>
+    <Table striped bordered hover>
+      <thead>
+      <tr>
+        <th>Name</th>
+        <th>Code</th>
+        <th>Rate</th>
+      </tr>
+      </thead>
+      <tbody>
+      {currencies.map(currency => <Currency currency={currency} key={currency.cc} />)}
+      </tbody>
+    </Table>
+  </>
+}
+
+export default Currencies;
---
+++ src/components/Currency.js
@@ -0,0 +1,9 @@
+function Currency({ currency }) {
+  return <tr>
+    <td>{currency.txt}</td>
+    <td>{currency.cc}</td>
+    <td>{currency.rate}</td>
+  </tr>;
+}
+
+export default Currency;
